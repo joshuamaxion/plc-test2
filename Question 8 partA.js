824048a113cd04fe4c44d17fc38f21f61018d987
joshuamaxion <36647442+joshuamaxion@users.noreply.github.com>
2021-04-21 23:28:42 -0400

    Add files via upload

---
+++ Question 8 partA.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<title>Web Page Design</title>
+<head>
+<script>
+function sayHello() {
+   document.write("Hello, Coding Ground!");
+   
+int set A{
+  let x=2;
+let y=5;
+let z=9;
+set C();
+}
+int set B{
+  let x=1;
+  let z= 16;
+  
+}
+int set C{
+  let y = 20;
+  let z = 4;
+  set B();
+}
+
+let x = 0;
+let y = 3;
+let z = 12;
+document.write(x,y,z);
+}
+sayHello();
+</script>
+</head>
+<body>
+</body>
+</html>
