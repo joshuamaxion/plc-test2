85f257321e6f26bb72616357cc76d02227f606b2
joshuamaxion <36647442+joshuamaxion@users.noreply.github.com>
2021-04-21 23:41:50 -0400

    Add files via upload

---
+++ Question8partB.py
@@ -0,0 +1,23 @@
+
+# Online Python - IDE, Editor, Compiler, Interpret
+int set A{
+ x=2;
+ y=5;
+ z=9;
+set C();
+}
+int set B{
+   x=1;
+  z= 16;
+  
+}
+int set C{
+   y = 20;
+   z = 4;
+  set B();
+}
+
+ x = 0;
+ y = 3;
+ z = 12;
+print %(x,y,z)
